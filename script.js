3b68334b8372419c828e55cf5761ee1e9e2cac10
ynnk99 <yalolp@gmail.com>
2026-07-01 11:08:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 const TWITCH_CLIENT_ID   = "n3oqt780bnsi3lb2gzinxdbrrazork";
 const TWITCH_REDIRECT_URI = window.location.origin + window.location.pathname;
-const APPS_SCRIPT_URL    = "https://script.google.com/macros/s/AKfycbzTt2y1Cgt7wzQpBGJC57LFa8B2o90MmkeJXuf83lDxC8aUyJPhzu6O_jJm4J65j5ri/exec";
-const TOOLBOX_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzTt2y1Cgt7wzQpBGJC57LFa8B2o90MmkeJXuf83lDxC8aUyJPhzu6O_jJm4J65j5ri/exec";
+const APPS_SCRIPT_URL    = "https://script.google.com/macros/s/AKfycbysgMpFsSRjmtOX__wIjwtB-ELoIDkwn8hnI8deRsuq-_OjLiejefpwQYERpzumyW1P/exec";
+const TOOLBOX_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbysgMpFsSRjmtOX__wIjwtB-ELoIDkwn8hnI8deRsuq-_OjLiejefpwQYERpzumyW1P/exec";
 
 const ALLOWED_USERS = [
   "ynnk99",
